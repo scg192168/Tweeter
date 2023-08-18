5df9235895e7bac78304aa4fccee99be4d60e9fe
Shyamal <scghoshbc@gmail.com>
2023-08-18 10:25:48 -0700

    form validation implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,79 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+$( document ).ready(function() {
+console.log("ready");
+// Function to create a tweet element
+const createTweetElement = function(tweet) {
+   return $(`
+    <article class="tweet">
+      <header>
+        <div class="tweet-header-left">
+          <img src="${tweet.user.avatars}" alt="avatar"/>
+          <span>${tweet.user.name}</span>
+        </div>
+        <p class="tweet-header-right">${tweet.user.handle}</p>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+      <span>${timeago.format(tweet.created_at)}</span>
+        <div class="icon-container">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+};
+
+// Function to render tweets
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
+// Function to lead tweets from the server
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: function(tweets) {
+      renderTweets(tweets);
+      console.log("loadTweets");
+    },
+    error: function(error) {
+      console.error('Error loading tweets:', error);
+    }
+  });
+}
+// Submit event handler for the form
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+console.log("insideFormHandler");
+    const $form = $(this);
+    const $textarea = $form.find('textarea');
+    const tweetText = $textarea.val().trim();
+
+    if (!tweetText) {
+      alert("Tweet content cannot be empty!");
+    } else if (tweetText.length > 140) {
+      alert("Tweet is too long! Maximum 140 characters.");
+    } else {
+    }
+  });
+
+// Load tweets from the server and render them
+loadTweets();
+console.log("ready!");
+});
+
+// Call the renderTweets function with the data array
+// renderTweets(data);
+
+// console.log( "ready!" );
+// });
